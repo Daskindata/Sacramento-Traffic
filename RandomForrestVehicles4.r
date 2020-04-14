4b82b61df9efe3eba921218e30652470c9bbd0f4
Daskindata <44386052+Daskindata@users.noreply.github.com>
2020-04-14 15:06:02 +1000

    David_Code

---
+++ RandomForrestVehicles4.r
@@ -0,0 +1,90 @@
+install.packages("disk.frame", repo="https://cran.rstudio.com/")
+
+library(forcats)
+install.packages('dplyr')
+library(MASS)
+library(randomForest)
+library(caret)
+library(reticulate)
+library(dplyr)
+sagemaker <- import('sagemaker')
+
+session <- sagemaker$Session()
+bucket <- session$default_bucket()
+
+inputData = read.csv(file = 'chunk4-vehicles.csv', header = TRUE, sep = ',')
+
+summary(inputData)
+
+inputData$trackedID <- as.numeric(inputData$trackedID)
+inputData$frame_num <- as.numeric(inputData$frame_num)
+inputData$bbox_xmin <- as.numeric(inputData$bbox_xmin)
+inputData$bbox_xmax <- as.numeric(inputData$bbox_xmax)
+inputData$bbox_ymin <- as.numeric(inputData$bbox_ymin)
+inputData$bbox_ymax <- as.numeric(inputData$bbox_ymax)
+inputData$num_objects_this_frame <- as.numeric(inputData$num_objects_this_frame)
+inputData$total_objects_detected <- as.numeric(inputData$total_objects_detected)
+
+#inputData$is_parked <- as.logical(inputData$is_parked)
+#inputData$is_parked <- as.numeric(inputData$is_parked)
+#inputData$is_double_parked <- as.logical(inputData$is_double_parked)
+#inputData$is_double_parked <- as.numeric(inputData$is_double_parked)
+
+set.seed(100) # set seed to replicate results
+trainingIndex <- sample(1:nrow(inputData), 0.5*nrow(inputData)) # indices for 60% training data
+trainingData <- inputData[trainingIndex, ] # training data
+testData <- inputData[-trainingIndex, ] # test data
+
+#trainingData_RF <- trainingData[, -c(1,2,3,5,6,7,8,9,10,11)]
+#trainingData_RF$zone_name <- levels(droplevels(trainingData_RF$zone_name))
+summary(trainingData)
+str(trainingData)
+str(testData)
+#summary(trainingData_RF)
+#str(trainingData_RF)
+
+set.seed(1234)# can be any number 
+#trainingData_RF <- droplevels(trainingData_RF)
+#trainingData_RF$zone_name <- droplevels(trainingData_RF$zone_name)
+#rf1 <- randomForest(is_double_parked ~ ., trainingData[1:883459,], importance=TRUE)
+#rf2 <- randomForest(zone_name ~ ., trainingData[883460:1766917,], importance=TRUE)
+#rf.combined <- combine(rf1,rf2)
+#rf.combined
+
+rf.model1 <- randomForest(is_double_parked ~., data = trainingData[1:800000,], na.action = na.omit, proximity=FALSE, importance = TRUE)
+rf.model1
+
+# Fine tuning parameters of Random Forest model
+rf.model2 <- randomForest(is_double_parked ~ ., data = trainingData[1:800000,], ntree = 500, mtry = 6, proximity = FALSE, importance = TRUE, na.action = na.omit)
+rf.model2
+
+trainset = trainingData[1:800000,]
+testset <- testData[1:340000,]
+predtrain = predict(rf.model2, trainset, type = "class")
+table(predtrain, trainset$is_double_parked)
+
+str(trainset)
+
+trainset = trainingData[1:800000,]
+testset <- testData[1:340000,]
+predvalid <- predict(rf.model2, testset, type = "class")
+mean(predvalid == testset$is_double_parked)
+table(predvalid, testset$is_double_parked)
+
+importance(rf.model2, descending)        
+varImpPlot(rf.model2)  
+
+# Using For loop to identify the right mtry for model
+a=c()
+i=5
+for (i in 3:8) {
+  model3 <- randomForest(is_double_parked ~ ., data = trainset, ntree = 500, mtry = i, importance = TRUE)
+  predValid <- predict(model3, testset, type = "class")
+  a[i-2] = mean(predValid == testset$is_double_parked)
+}
+ 
+a
+ 
+plot(3:8,a)
+
+
---
+++ carbike_lm-reducedmodel.R
@@ -0,0 +1,59 @@
+install.packages('aod')
+install.packages('ggplot2')
+install.packages('ISLR')
+install.packages('dplyr')
+install.packages('car')
+vehicleData <- read.csv("G:/David/JCU/CITY OF SACREMENTO DATA/CSV/chunk4-vehicles.csv", header = T, sep = ",")
+bicycleData <- read.csv("G:/David/JCU/CITY OF SACREMENTO DATA/CSV/chunk4-bicycles.csv", header = T, sep = ",")
+#pedestrianData <- read.csv("G:/David/JCU/CITY OF SACREMENTO DATA/CSV/chunk4-pedestrians.csv", header = T, sep = ",")
+#colnames(bicycleData)[11] <- "bike_class"  # rename response var
+#colnames(bicycleData)[12] <- "bike_zone"  # rename response var
+#vehicleData$bike_class <- bicycleData$bike_class
+sapply(bicycleData, class)
+
+vehicleData$is_parked <- as.logical(vehicleData$is_parked)
+vehicleData$is_parked <- as.numeric(vehicleData$is_parked)
+vehicleData$is_double_parked <- as.logical(vehicleData$is_double_parked)
+vehicleData$is_double_parked <- as.numeric(vehicleData$is_double_parked)
+
+vehicleData$trackedID <- as.numeric(vehicleData$trackedID)
+vehicleData$frame_num <- as.numeric(vehicleData$frame_num)
+vehicleData$bbox_xmin <- as.numeric(vehicleData$bbox_xmin)
+vehicleData$bbox_xmax <- as.numeric(vehicleData$bbox_xmax)
+vehicleData$bbox_ymin <- as.numeric(vehicleData$bbox_ymin)
+vehicleData$bbox_ymax <- as.numeric(vehicleData$bbox_ymax)
+vehicleData$num_objects_this_frame <- as.numeric(vehicleData$num_objects_this_frame)
+vehicleData$total_objects_detected <- as.numeric(vehicleData$total_objects_detected)
+
+bicycleData$trackedID <- as.numeric(bicycleData$trackedID)
+bicycleData$frame_num <- as.numeric(bicycleData$frame_num)
+bicycleData$bbox_xmin <- as.numeric(bicycleData$bbox_xmin)
+bicycleData$bbox_xmax <- as.numeric(bicycleData$bbox_xmax)
+bicycleData$bbox_ymin <- as.numeric(bicycleData$bbox_ymin)
+bicycleData$bbox_ymax <- as.numeric(bicycleData$bbox_ymax)
+bicycleData$num_objects_this_frame <- as.numeric(bicycleData$num_objects_this_frame)
+bicycleData$total_objects_detected <- as.numeric(bicycleData$total_objects_detected)
+
+
+#bikedat <- bicycleData[,11:12]
+
+install.packages('dpylr')
+library(dplyr)
+carbikeData <- full_join(vehicleData, bicycleData, by = c('x','frame_num'), all = TRUE)
+sapply(carbikeData, class)
+
+set.seed(100) # set seed to replicate results
+trainingIndex <- sample(1:nrow(carbikeData), 0.6*nrow(carbikeData)) # indices for 80% training data
+trainingData <- carbikeData[trainingIndex, ] # training data
+testData <- carbikeData[-trainingIndex, ] # test data
+
+str(carbikeData)
+summary(carbikeData)
+
+#Linear regression of the reduced model
+parked <- trainingData[, -c(1,2,3, 4, 6,7,8,9,10,12, 14,16,17,18,19,20,21,22,23,24)]
+lmMod2 <- lm(is_double_parked ~., parked, na.action = na.omit)  # the linear reg model
+summary(lmMod2) # get summary
+
+logitMOD_J <- glm(is_double_parked ~ total_objects_detected.x + classes.x + dwell_time_secs + zone_name.y, data = carbikeData, family = binomial(link = 'logit'))
+summary(logitMOD_J)
